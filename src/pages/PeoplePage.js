3a9b7e35735f570748ab88499a4f4dc5ed6cded2
Vitor Passos <vitorpassos16@gmail.com>
2021-03-15 23:33:01 -0300

    Loading and treat error api

--- src/pages/PeoplePage.js
+++ src/pages/PeoplePage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View } from 'react-native';
+import { View, ActivityIndicator, StyleSheet, Text } from 'react-native';
 import PeopleList from '../components/PeopleList';
 import axios from 'axios'
 
@@ -8,29 +8,59 @@ export default class PeoplePage extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            peoples: []
+            peoples: [],
+            loading: false,
+            error: false
         };
     }
 
     componentDidMount() {
+        this.setState({ loading: true })
         axios.get('https://randomuser.me/api/?nat=us&results=25').then(response => {
             const { results } = response.data;
             this.setState({
-                peoples: results
+                peoples: results,
+                loading: false,
+            })
+        }).catch(error => {
+            this.setState({
+                loading: false,
+                error: true
             })
         })
+    }
+    renderPage() {
+        if (this.state.loading) {
+            return <ActivityIndicator size="large" color="#348feb" />;
+        } if (this.state.error) {
+            return <Text style={styles.error}>Ops, Algo deu errado =(</Text>
+        }
+        return (<PeopleList peoples={this.state.peoples}
+            onPressItem={pageDetails => {
+                this.props.navigation.navigate('PeopleDetail', pageDetails);
+            }}
+        />)
     }
 
     render() {
         return (
-            <View>
-                <PeopleList peoples={this.state.peoples}
-                    onPressItem={pageDetails => {
-                        this.props.navigation.navigate('PeopleDetail', pageDetails);
-                    }}
-                />
+            <View style={styles.container}>
+                {this.renderPage()}
             </View>
         );
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    error: {
+        color: 'red',
+        alignSelf: 'center',
+        fontSize: 18
+    }
+
+})
 
